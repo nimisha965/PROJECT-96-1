a4400160dc0abeae552709742a3e7207064eac2b
nimisha965 <114502326+nimisha965@users.noreply.github.com>
2023-02-02 20:52:08 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+    //Set "player1_name" using localStorage.setItem() function
+	  //Set "player2_name" using localStorage.setItem() function
+    player1_name=localStorage.getItem("player1_name");
+player2_name=localStorage.getItem("player2_name");
+    window.location = "game_page.html";
+}
